f1acd3606ad20ac1e0c70eb046f011c901a88795
Noel Kirkland <noelkirkland@gmail.com>
2020-06-29 09:39:54 -0700

    reveived help brom Brittany and Sara to succesfully flesh ou the feed function
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

---
+++ src/feed-the-bear.js
@@ -0,0 +1,31 @@
+export class HungryBear {
+
+  constructor(name) {
+    this.name = name;
+    this.foodLevel = name;
+    this.foodLevel = 10;
+    this.moodLevel = 0;
+  }
+
+  setHunger() {
+    setInterval(() => {
+      this.foodLevel--;
+    }, 1000);
+  }
+
+  didYouGetEaten() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  feed() {
+    if (this.foodLevel > 0){
+      return this.foodLevel = 10;
+    }
+  }
+
+  setMood()
+}
